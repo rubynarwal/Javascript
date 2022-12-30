3d3da7487332385ab16facce412788925d5fc21f
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-30 10:55:01 +0530

    ruby

---
+++ Javascript Projects/navbar.js
@@ -0,0 +1,10 @@
+let navbar = () =>{
+    return `
+    <h2><a href="./index.html">Home</a></h2>
+    <h2><a href="./cart.html">Cart</a></h2>
+    <h2><a href="./checkout.html">Checkout</a></h2>
+    <h2><a href="./signup.html">Signup</a></h2>
+    <h2><a href="./login.html">Login</a></h2>`
+}
+
+export {navbar};
