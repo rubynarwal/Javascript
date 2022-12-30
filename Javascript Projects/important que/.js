14c934202436510c43cf0b8a10db3ae8fd99fc70
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-30 10:47:23 +0530

    ruby

--- Javascript Projects/important que/.js
+++ Javascript Projects/important que/.js
@@ -1,5 +1,5 @@
-//Given an array of numbers find the sum of cubes if the number is divisible by 3 Sample Input - [1, 2, 3, 4, 5, 6] Sample Output - 243 (27+216)
-let arr=[1,2,3,4,5,6];
+//Given an array of numbers find the sum of cubes if the number is divisible by 3 Sample Input - [1, 2, 3, 4, 5, 6,7] Sample Output - 243 (27+216)
+let arr=[1,2,3,4,5,6,7];
 let sum_of_cube=arr.filter(function(el){
   return el%3==0;
 }).map(function(sum,el){
