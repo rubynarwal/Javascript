c990bc038a6729f3cb3e90950beeb4c181fe21f6
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-30 10:48:12 +0530

    ruby

--- Javascript Projects/important que/q2.js
+++ Javascript Projects/important que/q2.js
@@ -1,4 +1,4 @@
-let arr=["assignment", "problem", "media", "upload"];
+let arr=["assignment", "Adventure", "Army", "upload"];
 let arr1=[];
 let resm=arr.filter(function(el){
   return(el[0]=="a"||el[el.length-1]=="a")
