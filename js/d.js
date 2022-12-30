496f72d779057a96e493128838175f64219dbd1e
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-30 20:52:00 +0530

    ruby

--- js/d.js
+++ js/d.js
@@ -2,6 +2,6 @@ function percentage(part, total) {
     return (part / total) * 100;
   }
   
-  console.log(percentage(50, 100)); // 50
+  console.log(percentage(40, 100)); // 50
   console.log(percentage(25, 50)); // 50
   console.log(percentage(75, 150)); // 50
