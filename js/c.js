934872883c33b5dcf11618daca523945579393de
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-12-30 20:54:37 +0530

    ruby

--- js/c.js
+++ js/c.js
@@ -2,4 +2,4 @@ const currentYear = 2022;
 const birthYear = prompt("What is your birth year?");
 const age = currentYear - birthYear;
 
-console.log(`You are ${age} years old.`);
+console.log(`You ${age} years old.`);
